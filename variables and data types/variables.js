5f10f6be2e32b82c2cb4165c613b9908cb7aa2e7
new divide <bistpramod113@gmail.com>
2026-01-29 14:38:49 +0545

    Add variables.js file

---
+++ variables and data types/variables.js
@@ -0,0 +1,72 @@
+// why do weuse let const and var and why do we not use var that much 
+
+
+
+//!data types in js 
+// 1. Primitive data types
+//  a. String
+//  b. Number   
+//  c. Boolean
+//  d. Null
+//  e. Undefined
+//  f. Symbol
+//  g. BigInt
+
+let str = "Hello, World!"; // String
+let num = 42; // Number
+let bool = true; // Boolean
+let n = null; // Null
+
+let u; 
+  // tempelate literal 
+  console.log(`The value of u is: ${num}`);
+
+
+
+  //number 
+
+  let a = 10;
+    let b = 3.14;
+    let c = -5; // 
+
+
+    //boolean its fo true and false
+    let d = true;
+    let e = false;
+
+
+  // undefined 
+ let l = undefined
+
+// or it can be written as
+let k; // no need to say undefined explicitly,
+
+
+// the use of undefined is to represent a variable that has been declared but not assigned a value yet.
+
+// null
+
+let m = null;
+// null is used to represent the intentional absence of any object value.
+
+console.log(l, m , n ); // "string"  // n is a variable declared above and its type is null
+
+// OPERATORS IN Js
+//operators are special symbols that perform operations on operands (values and variables).
+
+// 1. Arithmetic Operators
+let x = 10;
+let y = 5;
+
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+
+
+
+
+// 2. Non-primitive data types
+//  a. Object
+//  b. Array
+//  c. Function
+//  d. Date
+//  e. RegExp
